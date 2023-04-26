69ae32f03330cc049c08bb442e2c289f5ed27d2a
Suar Palazón Losa <spalazol@emeal.nttdata.com>
2023-04-26 12:01:02 +0200

    Initial Commit

---
+++ DerramaSPL/DerramaSPLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DerramaSPLApp.swift
+//  DerramaSPL
+//
+//  Created by Suar PL on 26/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DerramaSPLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
